934c8f1c539610bfe0ec7e5aaa31900997d6c938
soypaduano <sebastian.paduano.m@gmail.com>
2018-10-22 23:11:25 +0200

    finalizando ejemplo de fetch api - arrow functions

--- ejemploFetchApi/fetchapi/js/app.js
+++ ejemploFetchApi/fetchapi/js/app.js
@@ -1,6 +1,7 @@
 document.getElementById('txtBtn').addEventListener('click', cargarTXT);
 document.getElementById('jsonBtn').addEventListener('click', cargarJSON);
 document.getElementById('apiBTN').addEventListener('click', cargarAPI);
+document.getElementById('arrowFunctionBTN').addEventListener('click', arrowFunctions )
 
 function cargarTXT(){
     fetch('datos.txt').then(function(res){
@@ -46,9 +47,36 @@ function cargarAPI(){
                 </li>
                 `;
         });
-
         document.getElementById('resultado').innerHTML = html;
-        
     });
+}
 
+
+//Ejemplos de arrow functions;
+
+let aprendiendo = function(){
+    console.log("Aprendiendo las arrow functions");
+}
+
+aprendiendo = () => {
+    console.log("Apreniendo las arrow fnctins");
+}
+
+aprendiendo = ()  => "Apreniendo las arrow fnctins";
+
+//Pasando parametros
+aprendiendo = (tecnologia) => "Aprendiendo la " + tecnologia;
+//Si es solo un parametro, no necesita el parensetis
+aprendiendo = tecnologia => "Aprendiendo la " + tecnologia;
+//Cuando es mas de un parametro, si necesita parametro. 
+
+const productos = ['Disco', 'Camisa', 'Guitarra'];
+
+const cantidadProductos = productos.map(function(producto){
+    return producto.length;
+});
+
+
+function arrowFunctions(){
+    console.log(aprendiendo('Javascript'));
 }
