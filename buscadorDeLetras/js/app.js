f0b18dc3a1d6c3ea46ebe278cb52f26c9ed72448
soypaduano <sebastian.paduano.m@gmail.com>
2018-10-28 21:06:36 +0100

    añadiendo proyecto de buscador de letras con modulos

---
+++ buscadorDeLetras/js/app.js
@@ -0,0 +1,53 @@
+import {API} from './api.js';
+import * as UI from './interfaz.js'; //Mejor exportar solo el resultado
+
+console.log(UI);
+
+UI.formularioBuscar.addEventListener('submit', (e) => {
+    e.preventDefault();
+    //Obtenemos datos del formulario
+    const artista = document.querySelector('#artista').value;
+    const cancion = document.querySelector('#cancion').value;
+
+    console.log(artista);
+    console.log(cancion);
+
+    if(artista === '' || cancion === ''){
+        //No hacemos query a la rest API
+        UI.divMensajes.innerHTML = 'Error, todos los campso deben ser obligatorios';
+        UI.divMensajes.classList.add('error');
+        setTimeout(() => {
+            UI.divMensajes.innerHTML = '';
+            UI.divMensajes.classList.remove('error');
+        }, 2000)
+    } else {
+        //Hacemos la llamada a la API
+        const api = new API(artista, cancion);
+        api.consultarAPI()
+        .then(data => {
+            
+            if(data.respuesta.lyrics){
+                //La cancion existe
+                const letra = data.respuesta.lyrics;
+                UI.divResultado.textContent = letra;
+
+            } else {
+                UI.divMensajes.innerHTML = 'La cancion no existe, prueba con otra busqueda';
+                UI.divMensajes.classList.add('error');
+                setTimeout(() => {
+                    UI.divMensajes.innerHTML = '';
+                    UI.divMensajes.classList.remove('error');
+                    UI.formularioBuscar.reset();
+                }, 2000)
+
+
+            }
+
+
+
+
+        });
+    }
+
+});
+
