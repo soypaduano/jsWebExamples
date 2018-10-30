f82900b07994063c0006d011bb8db974bc086f20
soypaduano <sebastian.paduano.m@gmail.com>
2018-10-30 23:53:05 +0100

    finalizando proyecto de indexedBD

--- indexedDB/indexedDB/js/app.js
+++ indexedDB/indexedDB/js/app.js
@@ -25,6 +25,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
         DB = createDB.result;
         console.log(DB);
+        
+        mostrarCitas();
     }
 
     //este metodo solo corre una vez y es ideal para crear el schema de la bd 
@@ -84,10 +86,67 @@ document.addEventListener('DOMContentLoaded', () => {
 
     function mostrarCitas(){
         while(citas.firstChild){
-            citas.removeChild();
+            
         }
 
-        let objectStore = DB.transaction('citas').objectStore('citas');    }
+        let objectStore = DB.transaction('citas').objectStore('citas');
 
+        objectStore.openCursor().onsuccess = function(e){
+            let cursor = e.target.result;
+
+            if(cursor){
+                let citaHTML = document.createElement('li');
+                citaHTML.setAttribute('data-cita-id', cursor.value.key);
+                citaHTML.classList.add('list-group-item');
+                citaHTML.innerHTML = `<p class="font-weight-bold">Mascota: <span class="font-weight-normal">${cursor.value.mascota}</span></p>
+                <p class="font-weight-bold">Cliente: <span class="font-weight-normal">${cursor.value.cliente}</span></p>
+                <p class="font-weight-bold">Fecha: <span class="font-weight-normal">${cursor.value.fecha}</span></p>
+                <p class="font-weight-bold">Sintomas: <span class="font-weight-normal">${cursor.value.sintomas}</span></p>`
+
+                const botonBorrar = document.createElement('btn');
+                botonBorrar.classList.add('borrar', 'btn', 'btn-danger');
+                botonBorrar.innerHTML = '<span aria-hidden= "true">x</span>Borrar'
+                botonBorrar.onclick = borrarCita;
+                citaHTML.appendChild(botonBorrar);
+
+                citas.appendChild(citaHTML);
+                cursor.continue();   
+            } else {
+                if(!citas.firstChild){
+                headingAministra.textContent = "Agrega citas para comenzar";
+                let listado = document.createElement('p');
+                listado.classList.add('text-center');
+                listado.textContent = 'No hay registros';
+                citas.appendChild(listado);
+                } else {
+                    headingAministra.textContent = "Administra tus citas";
+                }
+            }
+        }
+    }
+
+    function borrarCita(e){
+        let citaID = e.target.parentElement.getAttribute('data-cita-id');
+        
+        let transaction = DB.transaction(['citas'], 'readwrite');
+        let objectStore = transaction.objectStore('citas');
+
+        let peticion = objectStore.delete(citaID);
+
+        TransitionEvent.oncomplete = () => {
+            e.target.parentElement.parentElement.removeChild(e.target.parentElement);
+
+            if(!citas.firstChild){
+                headingAministra.textContent = "Agrega citas para comenzar";
+                let listado = document.createElement('p');
+                listado.classList.add('text-center');
+                listado.textContent = 'No hay registros';
+                citas.appendChild(listado);
+                } else {
+                    headingAministra.textContent = "Administra tus citas";
+                }
+
+        }
+    }
 
 })
