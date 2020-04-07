0853ed723e83908e07a011e6f4590659b2ab6e23
soypaduano <sebastian.paduano.m@gmail.com>
2020-04-07 21:58:46 +0200

    añadiendo comentarios, comentando utils emoji

--- password-search-project/js/main.js
+++ password-search-project/js/main.js
@@ -1,10 +1,7 @@
 let timeStarted = false, sec = 0, password = "", confirmPassword = "", minutes = 0, seconds = 0;
 let gamePassed = false;
-//f respects
-let puedePulsarF = false; fPulsado = false;
 
-
-//TODO: Cada vez que hagamos algo bien, tenemos que ponerlo en la segunda posición
+let puedePulsarF = false; fPulsado = false; //f respects tool
 
 
 $(document).ready(function () {
@@ -38,7 +35,6 @@ function updateCorrectToErrorAgain(id){
     $('#' + id).prependTo('#error-list');
     $('#' + id).addClass('error animated bounceInUp');
   }
-
 }
 
 function inputTextChanged() {
@@ -159,7 +155,7 @@ function listenerToRegister(){
   });
 }
 
-
+//Correct error list
 let errorsToCheck = {
   "withOneNumber": {
     "function": () => {
@@ -321,6 +317,7 @@ let errorsTest = {
 
 
 //Utils
+/* No necesito está funcion más
 function isEmoji(str) {
   var ranges = [
       '(?:[\u2700-\u27bf]|(?:\ud83c[\udde6-\uddff]){2}|[\ud800-\udbff][\udc00-\udfff]|[\u0023-\u0039]\ufe0f?\u20e3|\u3299|\u3297|\u303d|\u3030|\u24c2|\ud83c[\udd70-\udd71]|\ud83c[\udd7e-\udd7f]|\ud83c\udd8e|\ud83c[\udd91-\udd9a]|\ud83c[\udde6-\uddff]|[\ud83c[\ude01-\ude02]|\ud83c\ude1a|\ud83c\ude2f|[\ud83c[\ude32-\ude3a]|[\ud83c[\ude50-\ude51]|\u203c|\u2049|[\u25aa-\u25ab]|\u25b6|\u25c0|[\u25fb-\u25fe]|\u00a9|\u00ae|\u2122|\u2139|\ud83c\udc04|[\u2600-\u26FF]|\u2b05|\u2b06|\u2b07|\u2b1b|\u2b1c|\u2b50|\u2b55|\u231a|\u231b|\u2328|\u23cf|[\u23e9-\u23f3]|[\u23f8-\u23fa]|\ud83c\udccf|\u2934|\u2935|[\u2190-\u21ff])' // U+1F680 to U+1F6FF
@@ -330,7 +327,7 @@ function isEmoji(str) {
   } else {
       return false;
   }
-}
+}*/
 
 function countUpperCaseChars(str) {
   var count=0,len=str.length;
@@ -350,5 +347,3 @@ document.addEventListener('keyup', (e) => {
   }
 }
 });
-
-//Emoji: 😅
